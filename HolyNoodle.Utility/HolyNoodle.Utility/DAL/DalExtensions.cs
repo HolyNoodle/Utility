ae100a45e398e4929eb6581da7de3b5f3e0af9a3
HolyNoodle <kbriffaut@gmail.com>
2016-09-05 15:37:57 +0200

    Line break removal

---
+++ HolyNoodle.Utility/HolyNoodle.Utility/DAL/DalExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HolyNoodle.Utility.DAL
+{
+    public static class IDalObjectExtensions
+    {
+        public static AzureDb BindingDb { get; set; }
+
+        public static async Task<bool> RefreshBinding(this IDalObject o, string name)
+        {
+            return await BindingDb.RefreshBinding(o, name);
+        }
+        public static async Task<bool> RefreshBindings(this IDalObject o)
+        {
+            return await BindingDb.RefreshBindings(o);
+        }
+        public static async Task<bool> RefreshBindings(this List<IDalObject> list, string name)
+        {
+            return await BindingDb.RefreshBindings(list, name);
+        }
+        public static async Task<bool> RefreshBindings(this List<IDalObject> list)
+        {
+            return await BindingDb.RefreshAllBindings(list);
+        }
+    }
+}
