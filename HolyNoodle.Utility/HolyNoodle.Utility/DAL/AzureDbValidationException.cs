fca2147a9b8a1e313c224a5f49b3713b94aa3ff6
HolyNoodle <kbriffaut@gmail.com>
2017-02-06 11:31:07 +0100

    fix refacto. Regenerate Nuget Package

--- HolyNoodle.Utility/HolyNoodle.Utility/DAL/AzureDbValidationException.cs
+++ HolyNoodle.Utility/HolyNoodle.Utility/DAL/AzureDbValidationException.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HolyNoodle.Utility.Common.DAL
+namespace HolyNoodle.Utility.DAL
 {
     public class AzureDbValidationException : Exception
     {
