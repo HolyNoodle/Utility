bbcab735db28b0ca0b2f34cdc690cd4078ce9687
HolyNoodle <kbriffaut@gmail.com>
2017-04-20 12:35:03 +0200

    Add reflexion helper for reflexion operation as IsValueType, IsEnumerable and GetGeneircTypeDefinition

---
+++ HolyNoodle.Utility/HolyNoodle.Utility/ReflexionHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolyNoodle.Utility
+{
+    public static class ReflexionHelper
+    {
+        public static bool IsValueType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return type.IsValueType || type == typeof(string);
+        }
+
+        public static Type GetGenericTypeDefintion(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            var generics = type.GetGenericArguments();
+            return generics.Count() > 0 ? generics[0] : null;
+        }
+
+        public static bool IsEnumarable(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return (type.GetInterface("IEnumerable") != null);
+        }
+    }
+}
